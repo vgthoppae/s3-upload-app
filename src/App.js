c10245d5bd1c4ab820cf38a8268b40e9cea5ed32
vthoppae <venkat.thoppae@gmail.com>
2022-12-20 11:17:51 -0500

    demo changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: 'xxx',
+    secretAccessKey: 'xxx',
+  },
+});
+
 function App() {
+  const sendDocument = async (document) => {
+    const { title, file } = document;
+
+    const uploadParams = {
+      Bucket: 'xxx',
+      Key: title,
+      Body: file,
+    };
+
+    try {
+      await s3Client.send(new PutObjectCommand(uploadParams));
+      alert('Successfully uploaded file.');
+    } catch (err) {
+      return alert('There was an error uploading your file: ', err.message);
+    }
+  };
+
+  const uploadFile = (evt) => {
+    evt.preventDefault();
+    const title = evt.target.title;
+    const fileUpload = document.getElementById('fileUpload');
+    let formData = new FormData();
+    formData.append('file', fileUpload.files[0]);
+    // formData.append('file', 'test content');
+
+    if (title.value) {
+      console.log('HERE', formData);
+      sendDocument({ title: title.value, file: formData });
+
+      title.value = '';
+    } else {
+      alert('Please fill out all fields');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>S3 File Upload Demo</h1>
+
+      <div>
+        <h4>Upload Document:</h4>
+        <form
+          onSubmit={uploadFile}
+          className="upload"
+          method="post"
+          encType="multipart/form-data"
         >
-          Learn React
-        </a>
-      </header>
+          <label>Title: </label>
+          <input type="text" name="title" />
+          <input id="fileUpload" type="file" name="file" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
